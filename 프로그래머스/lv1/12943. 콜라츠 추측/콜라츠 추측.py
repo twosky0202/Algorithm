cf6fd92a8b5034ad2b2a70337893fc072833fb30
twosky0202 <110607164+twosky0202@users.noreply.github.com>
2023-04-02 20:39:10 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,15 @@
+def solution(num):
+    answer = 0
+    while True:
+        if(num == 1):
+            return answer
+        elif(answer == 500):
+            return -1
+        elif(num % 2 == 1):
+            num = num * 3 + 1
+            answer += 1
+        elif(num % 2 == 0):
+            num /= 2
+            answer += 1
+    
+    return answer
