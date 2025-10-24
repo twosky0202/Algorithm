0f24cd7dc32536f8034729d8e89d4a048dca3686
이하늘 <110607164+twosky0202@users.noreply.github.com>
2025-10-24 23:11:16 +0900

    [Bronze III] Title: 학점계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+g = input()
+
+if(g=="A+"):
+    print(4.3)
+elif(g=="A0"):
+    print(4.0)
+elif(g=="A-"):
+    print(3.7)
+elif(g=="B+"):
+    print(3.3)
+elif(g=="B0"):
+    print(3.0)
+elif(g=="B-"):
+    print(2.7)
+elif(g=="C+"):
+    print(2.3)
+elif(g=="C0"):
+    print(2.0)
+elif(g=="C-"):
+    print(1.7)
+elif(g=="D+"):
+    print(1.3)
+elif(g=="D0"):
+    print(1.0)
+elif(g=="D-"):
+    print(0.7)
+elif(g=="F"):
+    print(0.0)
