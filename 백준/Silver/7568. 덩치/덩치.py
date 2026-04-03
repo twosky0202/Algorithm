f44b2b1d349b7491ecf81df57313e398751fe79e
이하늘 <110607164+twosky0202@users.noreply.github.com>
2026-04-03 21:14:29 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,13 @@
+n = int(input())
+students = []
+
+for i in range(n):
+    x, y = map(int, input().split())
+    students.append((x, y))
+
+for i in students:
+    rank = 1
+    for j in students:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end=" ")
