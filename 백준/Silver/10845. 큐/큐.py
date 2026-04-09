abbda701891001a0a5f428d4e2f053aa797a17e8
이하늘 <110607164+twosky0202@users.noreply.github.com>
2026-04-09 23:50:36 +0900

    [Silver IV] Title: 큐, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+queue = []
+
+for i in range(n):
+    s = list(input().split())
+
+    if s[0] == "push":
+        queue.append(int(s[1]))
+    elif s[0] == "pop":
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.pop(0))
+    elif s[0] == "size":
+        print(len(queue))
+    elif s[0] == "empty":
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif s[0] == "front":
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif s[0] == "back":
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
